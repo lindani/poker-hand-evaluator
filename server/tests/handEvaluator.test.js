9481eea273bb6929387c7c7e1afead189422d1b3
lindani <panilindani02@gmail.com>
2025-06-13 06:21:52 +0200

    feat(test): Add test for straight and normal flush hand rank

--- server/tests/handEvaluator.test.js
+++ server/tests/handEvaluator.test.js
@@ -22,6 +22,11 @@ describe('getBestHand', () => {
     expect(getBestHand(hand)).toBe('Three of a Kind');
   });
 
+  test('returns Flush for a hand with same suit', () => {
+    const hand = ['5H', '6H', '8H', '9H', '10H'];
+    expect(getBestHand(hand)).toBe('Flush');
+  });
+
   test('returns Full House for a hand with three 3s and two 2s', () => {
     const hand = ['3H', '3D', '3S', '2C', '2H'];
     expect(getBestHand(hand)).toBe('Full House');
@@ -30,5 +35,10 @@ describe('getBestHand', () => {
   test('returns Four of a Kind for a hand with four 10s and one 2s', () => {
     const hand = ['10H', '10D', '10S', '10C', '2D'];
     expect(getBestHand(hand)).toBe('Four of a Kind');
+  });
+
+  test('returns Flush for a hand with same suit', () => {
+    const hand = ['5D', '6D', '7D', '8D', '9D'];
+    expect(getBestHand(hand)).toBe('Straight Flush');
   });
 })
